2014b22a33c5c99e3959acff69a7bf0af82cbdf8
farxodnazarov <farhodnazarov002gmail.com>
2024-06-20 10:17:20 +0500

    data-json

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TripList from "./TripList.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <TripList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TripList.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import "./TripList.css";
+
+function TripList() {
+  const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
+
+  console.log(trips);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setTrips(data));
+  }, [url]);
+
+  return (
+    <div className="trip-list">
+      <h1>TripList</h1>
+      <ul>
+        {trips.map((trip) => {
+          return (
+            <li key={trip.title}>
+              <h2>{trip.title}</h2>
+              <p>{trip.price}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=Europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TripList;
